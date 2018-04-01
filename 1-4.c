d1533d791b44f4e5b626d2043590ac43eeeafa7e
summerzsy <1042380097@qq.com>
2018-04-01 22:13:24 +0800

    commit1-4

---
+++ 1-4.c
@@ -0,0 +1,8 @@
+# include <stdio.h>
+int main(void)
+{
+	int a=3.14;
+	printf("a的值是%d。\n",a);
+	return 0;
+}
+//因为int为整型所以只保留整数部分
--- dyc.cpp
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-int main(void)
-{
-   printf("*****/n  *****/n    *****/n      *****/n");
-   system("pause");
-   return 0;
-}
