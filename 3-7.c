2480ee3513f9ae147e7b39e0177675e8ab212ea5
summerzsy <1042380097@qq.com>
2018-04-02 16:54:46 +0800

    commit3-7

---
+++ 3-7.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(void)
+{
+    int a,b,c,d,max;
+    puts("请输入四个整数:");
+    printf("整数a:");      scanf("%d", &a);
+    printf("整数b:");      scanf("%d", &b);
+	printf("整数c:");      scanf("%d", &c);
+	printf("整数d:");      scanf("%d", &d);
+	max=a;
+    if (b>=max) max=b;
+	if (c>=max) max=c;
+	if (d>=max) max=d;
+        printf("最大值是:%d\n", max);
+    return 0;
+} 
