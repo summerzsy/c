76ce6be3112661f96a58d136ae31171115c8a96e
summerzsy <1042380097@qq.com>
2018-04-02 13:44:37 +0800

    commit2-1

---
+++ 2-1.c
@@ -0,0 +1,10 @@
+# include <stdio.h>
+int main(void)
+{
+	int x, y;
+	puts("请输入两个整数。");
+	printf("整数x:");  scanf("%d", &x);
+	printf("整数y:");  scanf("%d", &y);
+    printf("%.2lf %%\n", (double)x/y*100);
+	return 0;
+}
