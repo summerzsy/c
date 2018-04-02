2f40a082523f838be5da39b1767b72726b28e7ab
summerzsy <1042380097@qq.com>
2018-04-02 16:24:55 +0800

    commit3-3

---
+++ 3-3.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+
+
+int main(void)
+{
+    int a;
+    printf("请输入一个整数:");     scanf("%d", &a);
+    if ((a >= 0) == 1)
+        printf("绝对值是%d。\n", a);
+    else 
+        printf("绝对值是%d。\n", -a);
+    return 0;
+
+}       
